e1f70004cecdec676bdff145106e964b3e6e6906
Soroka0105 <soroka0105@gmail.com>
2023-06-15 13:23:52 +0300

    1

---
+++ script.js
@@ -0,0 +1,8 @@
+const BRAND_NAME = "SAMSUNG";
+const userInput = "saMsUng";
+const normalizedToUpperCaseInput = userInput.toUpperCase();
+
+console.log(userInput); // 'saMsUng'
+console.log(userInput === BRAND_NAME); // false
+console.log(normalizedToUpperCaseInput);
+console.log(normalizedToUpperCaseInput === BRAND_NAME);
